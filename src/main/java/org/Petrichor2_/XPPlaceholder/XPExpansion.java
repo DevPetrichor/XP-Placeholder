2c6c6d71f1ff630cfa9ce4e5ea55487e22e4d68d
Petrichor2_ <Peeters.rik09@gmail.com>
2025-06-18 22:40:22 +0200

    Initial commit

---
+++ src/main/java/org/Petrichor2_/XPPlaceholder/XPExpansion.java
@@ -0,0 +1,4 @@
+package org.Petrichor2_.XPPlaceholder;
+
+public class XPExpansion {
+}
---
+++ src/main/java/org/Petrichor2_/XPPlaceholder/XPPlaceholder.java
@@ -0,0 +1,17 @@
+package org.Petrichor2_.XPPlaceholder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XPPlaceholder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
